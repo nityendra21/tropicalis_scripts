1c6c417874de21928fb0f07363aff66c6b85d0d0
Nityendra Shukla <nitinshukla218@gmail.com>
2026-02-24 13:17:41 +0530

    Update run_cnv.sh

--- CNV/run_cnv.sh
+++ CNV/run_cnv.sh
@@ -6,9 +6,7 @@ mkdir -p ${CNV_OUTDIR}/{depth,windows,normalized,calls}
 
 while read SAMPLE; do
     bash depth_calculation.sh ${SAMPLE}
-    bash window_average.sh ${SAMPLE}
     bash normalize.sh ${SAMPLE}
-    bash call_cnv.sh ${SAMPLE}
 done < ../${SAMPLES}
 
 echo "CNV calling completed."
